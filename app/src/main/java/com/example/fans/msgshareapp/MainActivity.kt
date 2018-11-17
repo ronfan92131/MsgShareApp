60bf403c96d27d967a37a478430945f8bb52789e
Fans <fans@Fanss-iMac.local>
2018-11-16 20:06:01 -0800

    Initial commit

---
+++ app/src/main/java/com/example/fans/msgshareapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fans.msgshareapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MainActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
